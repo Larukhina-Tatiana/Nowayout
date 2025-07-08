b84c6f54274e597d0509b656eab25dda30b6215b
Татьяна <ltasya72@gmail.com>
2025-07-08 19:29:03 +0300

    исправлена логика скрытия навигации кнопок слайдера для mobile

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -7,8 +7,13 @@ import "swiper/css/navigation";
 let swiper;
 
 export function initRoomsSwiper() {
-  if (window.innerWidth <= 435) {
-    // отключаем стрелки и transform
+  if (
+    window.innerWidth <= 435 ||
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    // отключаем стрелки и transform на мобильных устройствах
     document.querySelector(".slider-nav.prev")?.classList.add("hidden");
     document.querySelector(".slider-nav.next")?.classList.add("hidden");
     const wrapper = document.querySelector(".rooms__cards .swiper-wrapper");
